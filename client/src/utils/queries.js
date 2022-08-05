529944a84f742b167842174d390e0e53f88098a5
raydover <105178034+raydover@users.noreply.github.com>
2022-08-04 23:15:46 -0400

    commit add to resolvers.js, require signtoken, AuthenticationError

---
+++ client/src/utils/queries.js
@@ -0,0 +1,9 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_PROFILES = gql`
+  query me {
+    profiles {
+      
+    }
+  }
+`;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User } = require('../models');
-const { AuthenticationError } = require('apollo-server-express')
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -10,41 +11,50 @@ const resolvers = {
                 });
                 return foundUser;
             }
+            throw new AuthenticationError('Your not logged in');
         },
     },
     Mutations: {
         addUser: async (parent, args) => {
-            const user = await User.create(body);
+            const user = await User.create(args);
             const token = signToken(user);
             return { token, user };
         },
-        login: async (parent, args) => {
-            const user = await User.findOne(args.email);
-            if (!user){
-                new AuthenticationError("User not found!")
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('User not found!')
             }
-            const correctPw = await user.isCorrectPassword(body.password);
+            const correctPw = await user.isCorrectPassword(password);
+            if (!password) {
+                throw new AuthenticationError('Password not found!')
+            }
             const token = signToken(user);
             return { token, user };
         },
-        saveBook: async (parent, args) => {
-            console.log(user);
-            const updatedUser = await User.findOneAndUpdate(
-                { _id: user._id },
-                { $addToSet: { savedBooks: body } },
-                { new: true, runValidators: true }
-            );
-            return updatedUser;
+        saveBook: async (parent, { book }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: book } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('Your not logged in!')
         },
-        removeBook: async (parent, args) => {
-            const updatedUser = await User.findOneAndUpdate(
-                { _id: user._id },
-                { $pull: { savedBooks: { bookId: params.bookId } } },
-                { new: true }
-            );
-            return updatedUser;
-        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('Your not logged in!')
+        }
     }
-}
+};
 
 module.exorts = resolvers;
