fe8d94cbbd59f8ad8eeee0d6968f9f1f46d8584f
raydover <105178034+raydover@users.noreply.github.com>
2022-08-06 08:21:53 -0400

    import correct requires for savedBooks.js

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
+import { GET_ME } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutaitons';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+import {useMutation, useQuery } from 'apollo/react-hooks';
 
 const SavedBooks = () => {
   const [userData, setUserData] = useState({});
